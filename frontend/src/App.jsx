a51a2c79859a330b2b35c73dbf189966d3ccc331
kinoshan <kinoshanketheeswaran01@gmail.com>
2026-02-15 05:07:30 +0530

    stage 1 completed login and registration auth is done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Signup from './Signup'
 import Login from './Login'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './assets/Home'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route path='/register' element={<Signup/>}> </Route>
           <Route path='/login' element={<Login/>}> </Route>
+          <Route path='/home' element={<Home/>}> </Route>
 
         </Routes>
       
